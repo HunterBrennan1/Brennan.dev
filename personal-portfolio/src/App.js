20653fe169a10c44a11b4e6168e48306dd6f86f8
Hunter <hunterbrennan15@gmail.com>
2022-10-29 19:23:47 -0400

    deployed application on netlify for testing

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,4 +1,3 @@
-import "../src/App.css";
 import { NavBar } from "./components/NavBar";
 import { MainPage } from "./components/main.js";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,6 +5,7 @@ import { SkillsPage } from "./components/skills.js";
 import { Projects } from "./components/projects.js";
 import { About } from "./components/about.js";
 import { Contact } from "./components/contact.js";
+import "../src/App.css";
 
 function App() {
   return (
