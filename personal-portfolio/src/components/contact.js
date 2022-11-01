a36036fa360d61af502c8199e3c37bed7defb216
Hunter <hunterbrennan15@gmail.com>
2022-10-31 21:37:34 -0400

    added details to project 3 (Primary keys rental project)

--- personal-portfolio/src/components/contact.js
+++ personal-portfolio/src/components/contact.js
@@ -12,8 +12,7 @@ export const Contact = () => {
         <Row>
           <Col>
             <div class="contact-container">
-              <form name="contact-v1" action="/contact" method="POST" data-netlify="true" onSubmit="submit">
-                {/* <input type="hidden" name="form-name" value="contact-v1" /> */}
+              <form name="contact-v1" method="POST" data-netlify="true" onSubmit="submit">
                 <h3 class="form-title">Lets Get In Touch!</h3>
 
                 <input type="text" id="name" name="name" placeholder="Your Name" required />
--- personal-portfolio/src/components/projects.js
+++ personal-portfolio/src/components/projects.js
@@ -13,6 +13,10 @@ import Reactimg from "../assets/img/reactimg.svg";
 import PortImgDark from "../assets/img/portfolio-img-dark.JPG";
 import PortImgLight from "../assets/img/portfolio-img-light.JPG";
 import PortImgMobile from "../assets/img/portfolio-img-mobile.JPG";
+import PrimaryKeys1 from "../assets/img/pkrimg1.JPG";
+import PrimaryKeys2 from "../assets/img/Pkrimg2.JPG";
+import PrimaryKeys3 from "../assets/img/pkrmobile.JPG";
+
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -177,14 +181,14 @@ export const Projects = () => {
                 <Carousel.Item>
                   <img
                     className="d-block w-1 carousel-img"
-                    src={Placeholder}
+                    src={PrimaryKeys1}
                     alt="First slide"
                   />
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
                     className="d-block w-1 carousel-img"
-                    src={Placeholder}
+                    src={PrimaryKeys2}
                     alt="Second slide"
                   />
 
@@ -192,7 +196,7 @@ export const Projects = () => {
                 <Carousel.Item>
                   <img
                     className="d-block w-1 carousel-img"
-                    src={Placeholder}
+                    src={PrimaryKeys3}
                     alt="Third slide"
                   />
 
@@ -202,12 +206,13 @@ export const Projects = () => {
               {/* DESCRIPTION */}
 
               <div className="project-content-container">
-                <h4>CURRENT PORTFOLIO</h4>
+                <h4>Primary Keys Rentals</h4>
                 <div className="proj-desc-btn-wrap">
-                  <p className="Project-desc-p">That's right! you are currently viewing this project as my current developer portfolio. This project really helped me understand React.js and other tools.</p>
+                  <p className="Project-desc-p pkr">Primary Keys Rentals is a rental car service that allows users to easily navigate and select rental options from a list of vehichles. The user is able to search vehicles by type, Leave a review, And when they have found the car they need, they can add the vehicle to their cart.<br></br>This application was deployed on hiroku, please allow a few moments for it to load!</p>
+                  <p className="Project-desc-p pkr">This application was built by a team of developers as a project with myself as the primary contributer to the front end development</p>
                   <div>
-                    <a href="https://github.com/HunterBrennan1/Brennan.dev" target="_blank" className="proj-btn-link">Repo Code</a>
-                    <a href="https://hunterbrennandev.netlify.app" target="_blank" className="proj-btn-link">Deployed App</a>
+                    <a href="https://github.com/TottoMoe/PrimaryKeysRentals" target="_blank" className="proj-btn-link">Repo Code</a>
+                    <a href="https://primary-keys-rentals.herokuapp.com/login" target="_blank" className="proj-btn-link">Deployed App</a>
                   </div>
                 </div>
                 <div><p>THIS PROJECT WAS BUILT WITH</p>
@@ -223,7 +228,8 @@ export const Projects = () => {
                       alt="skill image"
                     />
                     <img className="skill-img-proj" src={JS} alt="skill image" />
-                    <img className="skill-img-proj" src={Reactimg} alt="skill image" />
+                    <img className="skill-img-proj" src={NodeJs} alt="skill image" />
+                    <img className="skill-img-proj githubimg" src={Express} alt="skill image" />
                   </div>
                 </div>
               </div>
